e2e5f88aab6d6e16bca1a9ae97d1e0a7cf9328b6
Tamás Kovács <kovacs.tamas0408@gmail.com>
2026-03-29 17:04:38 +0200

    Add bookings table schema to database

---
+++ backend/src/db/schema/bookings.ts
@@ -0,0 +1,12 @@
+import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+import { barbers } from "./barbers.ts";
+
+export const bookings = pgTable("bookings", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  barberId: uuid("barberId")
+    .notNull()
+    .references(() => barbers.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  email: text("email").notNull(),
+  date: text("date").notNull(),
+  time: text("time").notNull(),
+});
